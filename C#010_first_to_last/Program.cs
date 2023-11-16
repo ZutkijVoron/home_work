4a3eba5f8ddf8442924c207ede4b7b4628b10a14
Святослав <zutkijvoron44@gmail.com>
2023-11-16 17:43:43 +0600

    add defect

---
+++ C#010_first_to_last/Program.cs
@@ -0,0 +1,8 @@
+﻿double numberA = Convert.ToInt32(Console.ReadLine()); ;
+for (Console.WriteLine("Enter three-digit number"),
+numberA = Convert.ToInt32(Console.ReadLine());
+(numberA > 99, numberA < 1000);
+numberA++)
+{
+    Console.WriteLine("eeee");
+}
