0d2f1034b570647bf32501f3323850eb02139e8f
Святослав <zutkijvoron44@gmail.com>
2023-11-16 17:03:14 +0600

    add bill

---
+++ C#009_bill/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("numder");
+double numberA = Convert.ToInt32(Console.ReadLine());
+double index = numberA;
+int count = 0;
+numberA = -numberA;
+while (count < (index * 2))
+{
+    Console.WriteLine(numberA);
+    numberA++;
+    count++;
+}
