1ace19e0a76c23ad8bb14a918a8e90c347fe38d3
Святослав <zutkijvoron44@gmail.com>
2023-11-16 13:26:35 +0600

    add if and else

---
+++ C#005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your name ");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Yay, it's Masha!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.Write(username);
+}
