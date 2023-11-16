8689f935bab85683247e855c431417bcfca1c078
Святослав <zutkijvoron44@gmail.com>
2023-11-16 13:01:38 +0600

    add div

---
+++ C#004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("numder 1");
+double numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("number 2");
+double numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(numberA / numberB);
